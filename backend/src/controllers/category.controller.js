ac41f7e63dab2b3baf7e8664490135aa399b0566
Dakshinpriya11 <dakshinpriya.ts@foodhub.com>
2026-01-09 12:23:00 +0530

    Implement category controller with CRUD operations

---
+++ backend/src/controllers/category.controller.js
@@ -0,0 +1,21 @@
+const service = require("../services/category.service");
+
+exports.createCategory = async (req, res) => {
+  await service.createCategory(req.body);
+  res.json({ message: "Category created" });
+};
+
+exports.getCategories = async (req, res) => {
+  const categories = await service.getCategories(req.query.menuId);
+  res.json(categories);
+};
+
+exports.updateCategory = async (req, res) => {
+  await service.updateCategory(req.params.categoryId, req.body.name);
+  res.json({ message: "Category updated" });
+};
+
+exports.deleteCategory = async (req, res) => {
+  await service.deleteCategory(req.params.categoryId);
+  res.json({ message: "Category deleted" });
+};
