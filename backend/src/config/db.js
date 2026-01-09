813af42bb363888c1da933bf7ff0d8b08d5fe739
Dakshin Priya <dakshinpriya.ts@foodhub.com>
2026-01-09 13:18:57 +0530

    test3

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
@@ -9,15 +9,4 @@ const db = mysql.createPool({
 });
 
 module.exports = db;
-=======
-const mysql = require("mysql2/promise");
 
-const db = mysql.createPool({
-  host: "localhost",
-  user: "menu_user",
-  password: "menu123",
-  database: "menu_manager"
-});
-
-module.exports = db;
->>>>>>> 62d7ef15f9eb5f723e21db59dca112ded226f0e0
