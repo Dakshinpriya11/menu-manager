93824f7da2c184503f5c54b50ce32261e40fdd82
Dakshin Priya <dakshinpriya.ts@foodhub.com>
2026-01-09 13:14:11 +0530

    test

--- backend/src/controllers/category.controller.js
+++ backend/src/controllers/category.controller.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const service = require("../services/category.service");
 
 exports.createCategory = async (req, res) => {
@@ -20,26 +20,4 @@ exports.deleteCategory = async (req, res) => {
   await service.deleteCategory(req.params.categoryId);
   res.json({ message: "Category deleted" });
 };
-=======
-const service = require("../services/category.service");
 
-exports.createCategory = async (req, res) => {
-  await service.createCategory(req.body);
-  res.json({ message: "Category created" });
-};
-
-exports.getCategories = async (req, res) => {
-  const categories = await service.getCategories(req.query.menuId);
-  res.json(categories);
-};
-
-exports.updateCategory = async (req, res) => {
-  await service.updateCategory(req.params.categoryId, req.body.name);
-  res.json({ message: "Category updated" });
-};
-
-exports.deleteCategory = async (req, res) => {
-  await service.deleteCategory(req.params.categoryId);
-  res.json({ message: "Category deleted" });
-};
->>>>>>> 62d7ef15f9eb5f723e21db59dca112ded226f0e0
--- backend/src/controllers/menu.controller.js
+++ backend/src/controllers/menu.controller.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const service = require("../services/menu.service");
 
 exports.createMenu = async (req, res) => {
@@ -43,49 +43,3 @@ exports.getMenuAvailability = async (req, res) => {
   const data = await service.getMenuAvailability(req.params.menuId);
   res.json(data);
 };
-=======
-const service = require("../services/menu.service");
-
-exports.createMenu = async (req, res) => {
-  const result = await service.createMenu(req.body);
-  res.json(result);
-};
-
-exports.getMenus = async (req, res) => {
-  const menus = await service.getMenus(req.query.outletId);
-  res.json(menus);
-};
-
-exports.updateMenuStatus = async (req, res) => {
-  await service.updateMenuStatus(req.params.menuId, req.body.active);
-  res.json({ message: "Status updated" });
-};
-
-exports.deleteMenu = async (req, res) => {
-  await service.deleteMenu(req.params.menuId);
-  res.json({ message: "Menu deleted" });
-};
-
-exports.setMenuTiming = async (req, res) => {
-  await service.setMenuTiming(req.params.menuId, req.body);
-  res.json({ message: "Timing set" });
-};
-
-exports.getActiveMenu = async (req, res) => {
-  const menu = await service.getActiveMenu(req.query);
-  res.json(menu);
-};
-
-exports.getMenuByOrderType = async (req, res) => {
-  const menu = await service.getMenuByOrderType(
-    req.params.menuId,
-    req.query.orderType
-  );
-  res.json(menu);
-};
-
-exports.getMenuAvailability = async (req, res) => {
-  const data = await service.getMenuAvailability(req.params.menuId);
-  res.json(data);
-};
->>>>>>> 62d7ef15f9eb5f723e21db59dca112ded226f0e0
